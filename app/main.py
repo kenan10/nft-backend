f9a6c44a39fd2d84b963c06dc367b235ccbbdce3
kenan10 <denis2014grom@gmail.com>
2022-08-29 13:14:40 +0300

    disable CORS checking

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
 from .routers import access_lists
-from fastapi.middleware.cors import CORSMiddleware
-import os
 
 app = FastAPI()
-
-origins = ["http://localhost:3000", "http://hootis.com"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(access_lists.router)
 
