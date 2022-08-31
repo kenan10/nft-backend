37f5f87fe8c9e030845e43c64154f1c2cccb8b3a
kenan10 <denis2014grom@gmail.com>
2022-08-31 18:53:28 +0300

    bug fix

--- app/db/database.py
+++ app/db/database.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://backend_user:{os.getenv('BACKEND_PASSWORD')}@localhost:3306/{os.getenv('MYSQL_DATABASE')}"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://backend_user:{os.getenv('BACKEND_PASSWORD')}@db:3306/{os.getenv('MYSQL_DATABASE')}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
