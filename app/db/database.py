c5ba7a133e95e63c476dc913a3d06e8736137638
kenan10 <denis2014grom@gmail.com>
2022-09-02 16:14:56 +0300

    increase pull_size

--- app/db/database.py
+++ app/db/database.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://backend_user:{os.getenv('BACKEND_PASSWORD')}@db:3306/{os.getenv('MYSQL_DATABASE')}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=0)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=2000)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
