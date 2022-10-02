e8a2493afeb9f47d9c1a6ff3b20e15634d3d5a2b
Your Name <you@example.com>
2022-10-02 19:48:54 +0300

    revert bug fixes

--- app/routers/blockchain.py
+++ app/routers/blockchain.py
@@ -32,7 +32,7 @@ router = APIRouter(prefix="/blockchain", tags=["blockchain"])
 
 @router.on_event("startup")
 async def startup_event():
-    web3 = Web3(Web3.HTTPProvider(os.getenv("MAINNET_RPC")))
+    web3 = Web3(Web3.HTTPProvider(os.getenv("RPC_URL")))
     contract_address = os.getenv("CONTRACT_ADDRESS")
     with open("./abi.json") as abi_file:
         abi = json.load(abi_file)
